aacddb9aa397322b6b3999d588d1b9381096e8cb
GalaKtiKos2004 <94827294+GalaKtiKos2004@users.noreply.github.com>
2024-09-13 00:33:43 +1000

    работа со скоростью через кривую

--- Assets/Scripts/PlayerJumper.cs
+++ Assets/Scripts/PlayerJumper.cs
@@ -1,42 +1,49 @@
 using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerJumper : MonoBehaviour
 {
     [SerializeField] private AnimationCurve _yAnimation;
-    [SerializeField] private float _height;
+    [SerializeField] private float _speed;
     [SerializeField] private float _duration;
+
+    private Rigidbody _rigidbody;
 
     private bool _isJumping = false;
 
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && _isJumping == false)
+        if (Input.GetKey(KeyCode.Space) && _isJumping == false)
         {
-            StartCoroutine(JumpByTime(_duration, _height));
+            StartCoroutine(JumpByTime(_duration, _speed));
             _isJumping = true;
         }
 
     }
 
-    private IEnumerator JumpByTime(float duration, float height)
+    private IEnumerator JumpByTime(float duration, float speed)
     {
         float expiredSeconds = 0f;
         float progress = 0f;
         float maxProgress = 1f;
-
-        Vector3 startPosition = transform.position;
 
         while (progress < maxProgress)
         {
             expiredSeconds += Time.deltaTime;
             progress = expiredSeconds / duration;
 
-            transform.position = startPosition + new Vector3(0f, _yAnimation.Evaluate(progress) * height, 0f);
+            _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, speed * _yAnimation.Evaluate(progress), 0f); 
 
             yield return null;
         }
 
         _isJumping = false;
+        _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, 0f, 0f);
     }
 }
