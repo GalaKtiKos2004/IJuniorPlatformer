07a2eb897be541be0db7764876fdbbcc21a3774a
GalaKtiKos2004 <94827294+GalaKtiKos2004@users.noreply.github.com>
2024-09-13 02:48:56 +1000

    Сделан прыжок через AnimationCurve

--- Assets/Scripts/PlayerJumper.cs
+++ Assets/Scripts/PlayerJumper.cs
@@ -1,49 +1,46 @@
 using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class PlayerJumper : MonoBehaviour
 {
     [SerializeField] private AnimationCurve _yAnimation;
-    [SerializeField] private float _speed;
+    [SerializeField] private float _height;
     [SerializeField] private float _duration;
-
-    private Rigidbody _rigidbody;
 
     private bool _isJumping = false;
 
-    private void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
-    }
-
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space) && _isJumping == false)
+        if (Input.GetKey(KeyCode.Space))
         {
-            StartCoroutine(JumpByTime(_duration, _speed));
-            _isJumping = true;
+            if (_isJumping == false)
+            {
+                StartCoroutine(JumpByTime(_duration, _height));
+                _isJumping = true;
+            }
         }
 
     }
 
-    private IEnumerator JumpByTime(float duration, float speed)
+    private IEnumerator JumpByTime(float duration, float height)
     {
         float expiredSeconds = 0f;
         float progress = 0f;
         float maxProgress = 1f;
+
+        Vector3 previousPosition = new Vector3(0f, transform.position.y, 0f);
 
         while (progress < maxProgress)
         {
             expiredSeconds += Time.deltaTime;
             progress = expiredSeconds / duration;
 
-            _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, speed * _yAnimation.Evaluate(progress), 0f); 
+            transform.Translate(new Vector3(0f, _yAnimation.Evaluate(progress) * height, 0f) - previousPosition);
+            previousPosition = new Vector3(0f, _yAnimation.Evaluate(progress) * height, 0f);
 
             yield return null;
         }
 
         _isJumping = false;
-        _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, 0f, 0f);
     }
 }
